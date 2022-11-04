7f01a0d20c733f0da97d3d01109fcee4f84eee1e
gialexan | Gilmar Romani <gialexan@student.42sp.org.br>
2022-11-04 14:06:41 +0100

    test empy args

---
+++ pipex/pipex.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 15:00:07 by gialexan          #+#    #+#             */
+/*   Updated: 2022/11/03 19:11:12 by gialexan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PIPEX_H
+#define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include "./libft/libft.h"
+
+typedef struct	s_data
+{
+	int		argc;
+	char	**argv;
+	char	**envp;
+	char	**paths;
+	char	**cmd;
+	char	*path;
+	char	*access;
+	int		file[2];
+	int		index;
+	
+}	t_data;
+
+#endif
